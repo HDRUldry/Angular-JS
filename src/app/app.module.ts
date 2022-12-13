1cc450c89884ef73a71f067ce9d5260473983f90
Uldry <alexandre.ghesquier@gmail.com>
2022-12-13 14:15:43 +0100

    Premier push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './details/details.component';
+import { ModelsComponent } from './models/models.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetailsComponent,
+    ModelsComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
